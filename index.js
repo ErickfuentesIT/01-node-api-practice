51dfe4635cefb1f9fad514a4a22333db7c7e6615
ErickfuentesIT <erickfuentes.it@gmail.com>
2025-10-22 16:18:57 -0600

    Setting up package json

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
+const slugify = require("slugify");
+
 const replaceTemplate = require("./modules/replaceTemplate");
 
 ///////////////////////////////////////////
@@ -28,14 +31,6 @@ const replaceTemplate = require("./modules/replaceTemplate");
 // });
 // console.log("Will read file!");
 
-const data2 = [
-  {
-    name: "Erick",
-    surname: "Fuentes",
-    Ocupation: "FullStack Software Developer",
-  },
-];
-
 ///////////////////////////////////////////
 /////// SERVER
 
@@ -53,6 +48,9 @@ const tempCard = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
